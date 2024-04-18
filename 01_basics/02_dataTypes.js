d9e78cadf19e5efaf1c6789ae8b36b75bff4d297
Rishav Raj <101454458+starsailor2@users.noreply.github.com>
2024-04-18 10:31:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3+3) 
+
+let name = "Rishav"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
