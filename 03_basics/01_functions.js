54f66ca852d2fac45a75872623730c2c5c1002bf
Rishav Raj <101454458+starsailor2@users.noreply.github.com>
2024-04-24 02:18:01 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,5 +29,37 @@ function loginuserMessege(username = "bot") {
     return `${username} just logged in`
 }
 
-console.log(loginuserMessege());
+// console.log(loginuserMessege());
 // console.log(loginuserMessege("Rishav"));
+
+// function calculateCartPrice(num1) { // when we use that we consider only the first paramter
+// function calculateCartPrice(...num1) { // we use ... operator before the parameters to take multiple values in a pass
+function calculateCartPrice(val1, val2, ...num1) { // if we use some parameter before ... operator then first argument will be taken by first parameter and so on then rest remaining argument will taken as ...parameter .
+    return num1
+}
+
+// console.log(calculateCartPrice(2,3,4,6)) 
+
+const user = {
+    username: "Rishav",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and the price is ${anyobject.price}`);
+}
+
+handleObject(user) // passing object inside a function
+handleObject({
+    username: "sam",
+    price: 500
+})
+
+const myNewArray = [200, 300, 500, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([300, 600, 900]));
